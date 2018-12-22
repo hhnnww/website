1b283a868ccaaf880498e575bcf15e7167bd3e9d
weihua <175990271@qq.com>
2018-12-22 20:41:49 +0800

    1

--- xiaodown/dribbble.py
+++ xiaodown/dribbble.py
@@ -1,5 +1,4 @@
 import sys
-sys.path.append('../script/')
 import os
 import re
 import google_translate
--- xiaodown/run.py
+++ xiaodown/run.py
@@ -1,5 +1,4 @@
 import sys
-sys.path.append('../xiaodown/')
 import dribbble
 import time
 import gc
